92f9d8c4d0430e5bf5822407a3a714db6305b04b
tangyi2019 <inryygy100@163.com>
2020-11-01 00:30:15 +0800

    模式完成页面视图

--- assets/script/game.js
+++ assets/script/game.js
@@ -43,6 +43,11 @@ cc.Class({
         game_prefabs: {
             type: cc.Prefab,
             default: null
+        },
+        //关卡页
+        page_prefab: {
+            type: cc.Prefab,
+            default: null
         }
     },
 
@@ -65,6 +70,8 @@ cc.Class({
     },
     //休闲模式
     relaxation_pattern() {
+        this.fan_hui_node.active = false;
+        this.nan_du_node.active = false;
         this.star_interface(false);
         this.creation_relaxation_prefabs();
     },
@@ -82,42 +89,39 @@ cc.Class({
     },
     //创建休闲模式预制物
     creation_relaxation_prefabs() {
+
         if (this.relaxation) {
-            this.xing_jie_dian.removeFromParent(false);
-            this.xing_jie_dian = null;
+            this.relaxation.removeFromParent(false);
+            this.relaxation = null;
         }
         this.relaxation = new cc.Node();
         this.relaxation.parent = this.relaxation_node;
-        this.relaxation_pattern = cc.instantiate(this.relaxation_prefabs);
-        this.relaxation_pattern.getComponent('pattern').ba_game_jiaoben_chuanjinlai(this);
-        this.relaxation.addChild(this.relaxation_pattern);
-        this.guan_ka_arr = [];
-        for (let i = 0; i < 5; i++) {
-            this.guan_ka_arr[i] = [];
-            for (let j = 0; j < 5; j++) {
-                this.guan_ka_arr[i][j] = this.creation_guan_ka();
-            }
+        let t_page_num = 10;
+        let t_pg = cc.find('page_node', this.node).getComponent(cc.PageView)
+        for (let i = 0; i < t_page_num; i++) {
+            let t_page = cc.instantiate(this.page_prefab);
+            //t_page.parent = this.relaxation;
+            t_pg.addPage(t_page);
+            t_page.getComponent('page').set_page_index(i);
+            t_page.getComponent('page').setGamejs(this);
+            t_page.x = i * 720;
+
         }
-        this.deposit_score_arr();
-        this.alter();
-    },
-    //创建一个存分数的数组
-    deposit_score_arr() {
-        let grade = cc.sys.localStorage.getItem('score');
-        if (grade === null) {
-            this.guan_ka_amount_arr = [];
-            for (let i = 0; i < this.guan_ka_arr.length; i++) {
-                this.guan_ka_amount_arr[i] = [];
-                for (let j = 0; j < this.guan_ka_arr[i].length; j++) {
-                    this.guan_ka_amount_arr[i][j] = null;
-                }
-            }
-            let jieguo = JSON.stringify(this.guan_ka_amount_arr);
-            cc.sys.localStorage.setItem('score', jieguo);
-        } else {
-            let m_grade = cc.sys.localStorage.getItem('score');
-            this.guan_ka_amount_arr = JSON.parse(m_grade);
-        }
+        this.relaxation_node.width = t_page_num * 720;
+        this.relaxation_node.heigth = 1280;
+
+        // this.relaxation_pattern = cc.instantiate(this.relaxation_prefabs);
+        // this.relaxation_pattern.getComponent('pattern').ba_game_jiaoben_chuanjinlai(this);
+        // this.relaxation.addChild(this.relaxation_pattern);
+        // this.guan_ka_arr = [];
+        // for (let i = 0; i < 5; i++) {
+        //     this.guan_ka_arr[i] = [];
+        //     for (let j = 0; j < 5; j++) {
+        //         this.guan_ka_arr[i][j] = this.creation_guan_ka();
+        //     }
+        // }
+        // this.deposit_score_arr();
+        // this.alter();
     },
     //创建游戏界面 附加一个关卡数
     creation_game_prefabs(guan_ka_shu, hang, lie) {
@@ -135,59 +139,6 @@ cc.Class({
 
 
 
-    },
-    //关卡预制物创建
-    creation_guan_ka() {
-        if (!this.xing_jie_dian) {
-            this.xing_jie_dian = new cc.Node();
-            this.xing_jie_dian.parent = this.relaxation;
-        }
-        let guan_ka = cc.instantiate(this.guan_ka_prefabs);
-        this.xing_jie_dian.addChild(guan_ka);
-        guan_ka.getComponent('guan_ka').ba_game_jiaoben_chuanjinlai(this);
-        return guan_ka;
-    },
-    //改变位置
-    alter() {
-        let beginning = -260;//上面的起点
-        let destination = 340;//下面的终点
-        let m_width = this.guan_ka_arr[0][0].getChildByName('guan_ka_bg').width;//110
-        let m_heigth = this.guan_ka_arr[0][0].getChildByName('guan_ka_bg').height;//110
-        let right_interval = 20;//往右的间隔
-        let down_interval = 50;//往下的间隔
-        let number = 0;//cc.sys.localStorage.getItem('class');
-        // if (number === null) {
-        //     cc.sys.localStorage.setItem('class', 0);
-        // }
-        // parseInt(number);
-        for (let i = 0; i < this.guan_ka_arr.length; i++) {
-            for (let j = 0; j < this.guan_ka_arr[i].length; j++) {
-                let x = beginning + j * (m_width + right_interval);
-                let y = destination - i * (m_heigth + down_interval);
-                this.guan_ka_arr[i][j].position = cc.v2(x, y);
-                let grade = cc.sys.localStorage.getItem('score');
-                let m_grade = JSON.parse(grade);
-                if (m_grade != null) {
-                    let cheng_ji = m_grade[i][j];
-                    this.guan_ka_arr[i][j].getComponent('guan_ka').guan_ka_chengji(cheng_ji);
-                }
-                this.guan_ka_arr[i][j].getComponent('guan_ka').guan_ka_label(++number, i, j, this.guan_ka_amount_arr);
-            }
-        }
-        // for (let i = this.guan_ka_arr.length - 1; i >= 0; i--) {
-        //     for (let j = this.guan_ka_arr[i].length - 1; j >= 0; j--) {
-        //         let x = beginning + j * (m_width + right_interval);
-        //         let y = destination + i * (m_heigth + down_interval);
-        //         this.guan_ka_arr[i][j].position = cc.v2(-x, y);
-        //         let grade = cc.sys.localStorage.getItem('score');
-        //         let m_grade = JSON.parse(grade);
-        //         if (m_grade != null) {
-        //             let cheng_ji = m_grade[i][j];
-        //             this.guan_ka_arr[i][j].getComponent('guan_ka').guan_ka_chengji(cheng_ji);
-        //         }
-        //         this.guan_ka_arr[i][j].getComponent('guan_ka').guan_ka_label(++number, i, j, this.guan_ka_amount_arr);
-        //     }
-        // }
     },
     //隐藏休闲模式背景
     hide_relaxation(no_off) {
---
+++ assets/script/page.js
@@ -0,0 +1,100 @@
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        //关卡预制物
+        guan_ka_prefabs: {
+            type: cc.Prefab,
+            default: null
+        },
+        pageIndex: 0
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+    start() {
+        //this.shan_chu_ji_lv();
+        this.creation_relaxation_prefabs();
+    },
+    set_page_index(idx) {
+        this.pageIndex = idx;
+    },
+    //创建休闲模式预制物
+    creation_relaxation_prefabs() {
+        if (this.xing_jie_dian) {
+            this.xing_jie_dian.removeFromParent(false);
+            this.xing_jie_dian = null;
+        }
+        this.xing_jie_dian = new cc.Node();
+        this.xing_jie_dian.parent = this.node;
+
+        this.guan_ka_arr = [];
+        for (let i = 0; i < 5; i++) {
+            this.guan_ka_arr[i] = [];
+            for (let j = 0; j < 5; j++) {
+                this.guan_ka_arr[i][j] = this.creation_guan_ka();
+            }
+        }
+        this.deposit_score_arr();
+        this.alter_position();
+    },
+    //创建一个存分数的数组
+    deposit_score_arr() {
+        let score_str = 'score' + this.pageIndex;
+        let grade = cc.sys.localStorage.getItem(score_str);
+        if (grade === null) {
+            this.guan_ka_amount_arr = [];
+            for (let i = 0; i < this.guan_ka_arr.length; i++) {
+                this.guan_ka_amount_arr[i] = [];
+                for (let j = 0; j < this.guan_ka_arr[i].length; j++) {
+                    this.guan_ka_amount_arr[i][j] = null;
+                }
+            }
+            let jieguo = JSON.stringify(this.guan_ka_amount_arr);
+            cc.sys.localStorage.setItem(score_str, jieguo);
+        } else {
+            let m_grade = cc.sys.localStorage.getItem(score_str);
+            this.guan_ka_amount_arr = JSON.parse(m_grade);
+        }
+    },
+    setGamejs(game_js) {
+        this.game = game_js;
+    },
+    //关卡预制物创建
+    creation_guan_ka() {
+        let guan_ka = cc.instantiate(this.guan_ka_prefabs);
+        this.xing_jie_dian.addChild(guan_ka);
+        guan_ka.getComponent('guan_ka').ba_game_jiaoben_chuanjinlai(this.game);
+        return guan_ka;
+    },
+    //改变位置
+    alter_position() {
+        let beginning = 100;//上面的起点
+        let destination = 340;//下面的终点
+        let m_width = this.guan_ka_arr[0][0].getChildByName('guan_ka_bg').width;//110
+        let m_heigth = this.guan_ka_arr[0][0].getChildByName('guan_ka_bg').height;//110
+        let right_interval = 20;//往右的间隔
+        let down_interval = 50;//往下的间隔
+        let number = 0;//cc.sys.localStorage.getItem('class');
+        // if (number === null) {
+        //     cc.sys.localStorage.setItem('class', 0);
+        // }
+        // parseInt(number);
+        for (let i = 0; i < this.guan_ka_arr.length; i++) {
+            for (let j = 0; j < this.guan_ka_arr[i].length; j++) {
+                let x = beginning + j * (m_width + right_interval);
+                let y = destination - i * (m_heigth + down_interval);
+                this.guan_ka_arr[i][j].position = cc.v2(x, y);
+                let grade = cc.sys.localStorage.getItem('score');
+                let m_grade = JSON.parse(grade);
+                if (m_grade != null) {
+                    let cheng_ji = m_grade[i][j];
+                    this.guan_ka_arr[i][j].getComponent('guan_ka').guan_ka_chengji(cheng_ji);
+                }
+                this.guan_ka_arr[i][j].getComponent('guan_ka').guan_ka_label(++number, i, j, this.guan_ka_amount_arr);
+            }
+        }
+    },
+});
